2722cc79c5c5cbda17e5882f284aa04673eccf8d
Bharatjagoar <bharatjagoar15@gmail.com>
2025-08-10 06:59:48 +0530

    setting up server , added data in memory

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+// ======== In-memory data source ========
+let users = [
+    { id: "1", firstName: "Anshika", lastName: "Agarwal", hobby: "Teaching" },
+    { id: "2", firstName: "Bharat", lastName: "Jagoar", hobby: "Coding" }
+]; 
+
+// GET /users – Fetch all users
+app.get('/users', (req, res) => {
+    res.status(200).json(users);
+});
+
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
